21097e7186ad810ffeb4e7657568dcc102b4876b
wooyongjeong <wooyong.dev@gmail.com>
2022-01-13 16:54:14 +0900

    [영속성 관리 - 내부 동작 방식] 플러시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
@@ -17,49 +16,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            //엔티티 조회, 1차 캐시
-//            //비영속
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
-//
-//            //영속
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
-//
-//            Member findMember = em.find(Member.class, 101L);
-//            System.out.println("findMember.id = " + findMember.getId());
-//            System.out.println("findMember.name = " + findMember.getName());
-//
-//            tx.commit();
+            //영속
+            Member member = new Member(200L, "member200");
+            em.persist(member);
 
-            // ==========
-
-//            //1차 캐시에서 조회
-//            Member findMember1 = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L);
-//            //영속 엔티티의 동일성 보장
-//            System.out.println("result = " + (findMember1 == findMember2));
-
-            // ==========
-
-//            //엔티티 등록. 트랜잭션을 지원하는 쓰기 지연
-//            //영속
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-//            System.out.println("==============");
-//
-//            tx.commit();
-
-            // ==========
-
-            //엔티티 수정. 변경 감지 (Dirty Checking)
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
+            em.flush();
 
             System.out.println("================");
             tx.commit();
