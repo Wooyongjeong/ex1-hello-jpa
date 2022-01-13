87f71c1c683663bf69193076dbb3f8aea1bf3281
wooyongjeong <wooyong.dev@gmail.com>
2022-01-13 17:10:38 +0900

    [영속성 관리 - 내부 동작 방식] 준영속 상태

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,11 +17,15 @@ public class JpaMain {
         tx.begin();
 
         try {
+            //준영속 상태로 만드는 방법
             //영속
-            Member member = new Member(200L, "member200");
-            em.persist(member);
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
 
-            em.flush();
+//            em.detach(member); //JPA에서 얘를 관리를 안하게 되는거임
+            em.clear(); //영속성 컨텍스트를 싹 지워버림
+
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("================");
             tx.commit();
