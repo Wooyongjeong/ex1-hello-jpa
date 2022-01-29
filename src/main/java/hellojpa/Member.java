65b351d16ca376574c80b6eebe245973f5038d8b
wooyongjeong <wooyong.dev@gmail.com>
2022-01-29 21:32:48 +0900

    [엔티티 매핑] 데이터베이스 스키마 자동 생성

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -8,6 +9,8 @@ public class Member {
 
     @Id
     private Long id;
+
+    @Column(unique = true, length = 10)
     private String name;
 
     public Long getId() {
