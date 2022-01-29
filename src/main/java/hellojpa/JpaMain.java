ba708bd29f921b5bf20f2b5c6bfdfa4cd9892d65
wooyongjeong <wooyong.dev@gmail.com>
2022-01-30 01:23:55 +0900

    [연관관계 매핑 기초] 양방향 연관관계와 연관관계의 주인 1- 기본

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,21 +17,6 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            //객체를 테이블에 맞춰 모델링
-//            Team team = new Team();
-//            team.setName("TeamA");
-//            em.persist(team);
-//
-//            Member member = new Member();
-//            member.setUsername("member1");
-//            member.setTeamId(team.getId());
-//            em.persist(member);
-//
-//            Member findMember = em.find(Member.class, member.getId());
-//            Long findTeamId = findMember.getTeamId();
-//            Team findTeam = em.find(Team.class, findTeamId);
-
-            //객체 지향 모델링
             Team team = new Team();
             team.setName("teamA");
             em.persist(team);
@@ -40,9 +26,15 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
+            em.flush();
+            em.clear();
+
             Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+
+            List<Member> members = findMember.getTeam().getMembers();
+            for (Member m : members) {
+                System.out.println("m = " + m.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
