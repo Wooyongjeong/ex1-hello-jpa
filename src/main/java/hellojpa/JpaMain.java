90562e871b8ac0467f09bb774b607a4b9d935424
wooyongjeong <wooyong.dev@gmail.com>
2022-02-17 00:40:55 +0900

    [고급 매핑] 상속관계 매핑 - 구현 클래스마다 테이블 전략

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -29,8 +29,8 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("findMovie = " + findMovie);
+            Item item = em.find(Item.class, movie.getId());
+            System.out.println("item = " + item);
 
             tx.commit();
         } catch (Exception e) {
